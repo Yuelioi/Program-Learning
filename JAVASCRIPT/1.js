e461ba444da9d1da1a647d459dbc98bd7b5f883d
yueloi <yuelioi1210@gmail.com>
2022-09-12 21:41:26 +0800

    '20220912214126'

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -1,30 +1,41 @@
 // @start
-// @name:len
-// @description:获取数组长度
-const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+// @name:concat
+// @description:The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
 
-console.log(clothing.length);
-// expected output: 4
-
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
 // @end
 
 
 // @start
-// @name:len2
-// @description:获取数组长度2
-const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+// @name:copyWithin
+// @description:The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.
+const array1 = ['a', 'b', 'c', 'd', 'e'];
 
-console.log(clothing.length);
-// expected output: 4
+// copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
+
+// copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// expected output: Array ["d", "d", "e", "d", "e"]
 
 // @end
 
 // @start
-// @name:len3
-// @description:获取数组长度3
-const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+// @name:entries
+// @description:The entries() method returns a new Array Iterator object
+const array1 = ['a', 'b', 'c'];
 
-console.log(clothing.length);
-// expected output: 4
+const iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
 
 // @end
