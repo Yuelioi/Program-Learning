b5f1cba9ed4e5145672a7e8cfe60a1d4eb3db843
yueloi <yuelioi1210@gmail.com>
2023-02-23 03:36:43 +0800

    update

---
+++ Python/modules/_asyncio.py
@@ -0,0 +1,2 @@
+# https://zhuanlan.zhihu.com/p/158641367
+https: // www.bilibili.com/video/BV1oa411b7c9/
--- Python/modules/web/_playwright.py
+++ Python/modules/web/_playwright.py
@@ -1,38 +1,49 @@
 # 同步调用
+import asyncio
+from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright
 import time
 
-with sync_playwright() as p:
-   browser = p.chromium.launch(
-       executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False)
-   page = browser.new_page()
-   page.goto("https://blog.csdn.net/u013408061/article/details/53048291")
-   time.sleep(3)
-   page.evaluate("""() => {
-            document.querySelector('#blogColumnPayAdvert').style.display = 'none';
-               }""")
-   page.evaluate("""() => {
-      document.querySelector('.toolbar-btn-login').remove();
-            }""")
-   time.sleep(500)
 
-   # 获取所有li 使用locator
-   # top = page.locator(".f-weibo")
-   # top.screenshot(path="screenshot1.png")
+def main1():
 
-   # browser.close()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False)
+        page = browser.new_page()
+        page.goto("https://blog.csdn.net/u013408061/article/details/53048291")
+        time.sleep(3)
+
+        # 执行脚本
+        page.evaluate("""() => {
+                document.querySelector('#blogColumnPayAdvert').style.display = 'none';
+                }""")
+
+        time.sleep(500)
+
+        # 获取所有li 使用locator
+        # top = page.locator(".f-weibo")
+        # top.screenshot(path="screenshot1.png")
+
+        # browser.close()
+
 
 # 异步调用
-# from playwright.async_api import async_playwright
 
 
-# async def main():
-#     async with async_playwright() as playwright:
-#         browser = await playwright.launch(headless=True)
-#         context = await browser.new_context()
-#         page = await context.new_page()
+async def main2():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False)
+        page = await browser.new_page()
+        await page.goto('https://laplace.live/user/4279370')
+        # 异步等待
+        await asyncio.sleep(4)
+        top = page.locator('main [class*="Home_xl"]')
 
-#         await page.goto('https://www.example.com')
-#         print(f'Title of the page: {await page.title()}')
+        # 使用.nth(0) 获取第一个元素
+        await top.nth(0).screenshot(path="screenshot2.png")
+        await browser.close()
 
-#         await browser.close()
+
+if __name__ == '__main__':
+    asyncio.run(main2())
