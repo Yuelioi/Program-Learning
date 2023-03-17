5418594ed81feeecf90f1404cd2fc39f1add95fc
yuelioi <yuelioi1210@gmail.com>
2023-03-17 18:33:01 +0800

    update

--- Python/modules/web/_playwright.py
+++ Python/modules/web/_playwright.py
@@ -11,15 +11,14 @@ def main1():
         browser = p.chromium.launch(
             executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False)
         page = browser.new_page()
-        page.goto("https://blog.csdn.net/u013408061/article/details/53048291")
+        page.goto(
+            "https://www.behance.net/gallery/161360299/Cosmoses-?tracking_source=project_owner_other_projects")
         time.sleep(3)
 
         # 执行脚本
         page.evaluate("""() => {
                 document.querySelector('#blogColumnPayAdvert').style.display = 'none';
                 }""")
-
-        time.sleep(500)
 
         # 获取所有li 使用locator
         # top = page.locator(".f-weibo")
@@ -33,16 +32,23 @@ def main1():
 
 async def main2():
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False)
+        browser = await playwright.chromium.launch(executable_path=r"C:\Program Files\Google\Chrome\Application\chrome.exe", headless=False, proxy={"server": "http://127.0.0.1:10809"})
         page = await browser.new_page()
-        await page.goto('https://laplace.live/user/4279370')
-        # 异步等待
-        await asyncio.sleep(4)
-        top = page.locator('main [class*="Home_xl"]')
 
+        await page.goto('https://www.behance.net/gallery/161360299/Cosmoses-?tracking_source=project_owner_other_projects')
+        # 异步等待
+
+        await page.goto('https://www.behance.net/gallery/161360299/Cosmoses-?tracking_source=project_owner_other_projects')
+        # 异步等待
+
+        cover_element = await page.wait_for_selector('meta[property="og:image"]', timeout=10000)
+        print(1223)
+        if cover_element:
+            cover_url = await cover_element.get_attribute('content')
+            print(cover_url)
         # 使用.nth(0) 获取第一个元素
-        await top.nth(0).screenshot(path="screenshot2.png")
-        await browser.close()
+        # await top.nth(0).screenshot(path="screenshot2.png")
+        # await browser.close()
 
 
 if __name__ == '__main__':
