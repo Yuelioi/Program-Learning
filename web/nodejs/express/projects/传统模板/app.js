b61997652e22b44d3508299b29d7dad329f68da6
yueloi <yuelioi1210@gmail.com>
2023-05-12 02:39:09 +0800

    update

--- web/nodejs/express/projects/传统模板/app.js
+++ web/nodejs/express/projects/传统模板/app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 
+const { promisify } = require("util");
+
 // 渲染.art结尾的资源, 使用art-template渲染
 app.engine("art", require("express-art-template"));
 
@@ -26,7 +28,15 @@ const todos = [
     { id: 4, title: "吃饭4" },
 ];
 
-// routes
+// *托管静态资源
+// 访问时, 不需要加public了 => ./css/main.css
+app.use(express.static(path.join(__dirname, "./public")));
+
+// 访问时, 需要加public了 => ./public/css/main.css
+// 可以不是public 比如是static  => ./static/css/main.css (文件还是在public内)
+app.use("/public", express.static(path.join(__dirname, "./public")));
+
+// * routes
 app.get("/", function (req, res) {
     res.render("index", {
         foo: "bar",
@@ -34,6 +44,13 @@ app.get("/", function (req, res) {
     });
 });
 
+// 加载静态资源(手动)
+// app.get("/publi/css/main.css",(req, res)=>{
+//     const data = await promisify(fs.readFile("./publi/css/main.css"))
+//     res.send(data)
+// })
+
+// 加载页面(手动)
 // app.get("/", (req, res) => {
 //     fs.readFile("./views/index.html", "utf8", (err, templateStr) => {
 //         if (err) {
