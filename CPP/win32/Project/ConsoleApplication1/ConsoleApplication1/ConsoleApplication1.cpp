1cf46e5ada28acf0746ecd80fdb3bb466c22e8fa
yueloi <yuelioi1210@gmail.com>
2023-06-07 10:08:14 +0800

    update

---
+++ CPP/win32/Project/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Psapi.h>
+#include <Windows.h>
+
+#include <string>
+
+
+int main()
+{
+
+    
+
+}
+
+std::wstring GetActiveWindowProcessName() {
+    HWND activeWindow = GetForegroundWindow();
+    if (activeWindow != nullptr) {
+        DWORD processId;
+        GetWindowThreadProcessId(activeWindow, &processId);
+
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+            FALSE, processId);
+        if (hProcess != nullptr) {
+            wchar_t processName[MAX_PATH];
+            if (GetProcessImageFileName(hProcess, processName, MAX_PATH) > 0) {
+                std::wstring appName = processName;
+                size_t pos = appName.find_last_of(L"\\");
+                if (pos != std::wstring::npos) {
+                    appName = appName.substr(pos + 1);
+                }
+                CloseHandle(hProcess);
+                return appName;
+            }
+
+            CloseHandle(hProcess);
+        }
+    }
+
+    return L"";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
